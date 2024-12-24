97d80741b193587dec5cc80747db5626a612d4fa
popka <drensymax@gmail.com>
2024-12-24 14:32:07 +0300

    24.12.24 рабочая стабильная версия: - настроена зависимость com.github.bumptech.glide:glide:4.16.0 для воспроизведения гифки

--- app/src/main/java/com/example/chemcraft/MainActivity.kt
+++ app/src/main/java/com/example/chemcraft/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.chemcraft
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets//
-        }
+
+        // Инициализация элементов с использованием findViewById
+        val titleTextView = findViewById<TextView>(R.id.titleTextView)
+        val contentTextView = findViewById<TextView>(R.id.contentTextView)
+        val exampleImageView = findViewById<ImageView>(R.id.exampleImageView)
+
+
+        // Задаем данные
+        titleTextView.text = "Пример заголовка"
+        contentTextView.text = "Это пример текста, который можно заменить на ваш контент. Он поддерживает длинные абзацы и прокрутку."
+        exampleImageView.setImageResource(R.drawable.pnggog) // Убедитесь, что sample_image добавлен в папку res/drawable
+
+        val gifImageView: ImageView = findViewById(R.id.gifImageView)
+
+        Glide.with(applicationContext)
+          .load(R.drawable.image1)
+          .into(gifImageView)
     }
 }
